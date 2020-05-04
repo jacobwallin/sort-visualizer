65e372a365e94f90cbc365a545a79cc660666dd7
Jacob Wallin <jacobwallin@gmail.com>
2020-05-04 00:07:30 -0500

    bubble sort and merge sort working, UI updates

--- script.js
+++ script.js
@@ -1,66 +1,16 @@
-const BAR_SPACING = 1;
-let sortArray = [];
-let sorting = false;
-let startButton = document.getElementById("bubble-sort");
-startButton.addEventListener("click", () => {
-  bubbleSort();
-  console.log(state);
-});
-let mergeSortButton = document.getElementById("merge-sort");
-mergeSortButton.addEventListener("click", () => {
-  noLoop();
-  console.log("before Merge", sortArray);
-  mergeSort(0, sortArray.length - 1);
-  console.log("after Merge", sortArray);
-});
-
-function createRandomArray(length) {
-  sortArray = [];
-  for (let i = 0; i < length; i++) {
-    sortArray.push(Math.random());
-  }
-  count1 = sortArray.length - 1;
-  count2 = 0;
-}
-
 function bubbleSort() {
+  snapshot();
   for (let i = sortArray.length - 1; i >= 0; i--) {
     for (let j = 0; j < i; j++) {
-      saveState();
       if (sortArray[j] > sortArray[j + 1]) {
         // swap numbers
         let temp = sortArray[j];
         sortArray[j] = sortArray[j + 1];
         sortArray[j + 1] = temp;
       }
+      snapshot();
     }
   }
-}
-
-let state = [];
-function saveState() {
-  state.push(sortArray.map((value) => value));
-}
-
-function bubbleSortStep(i, j) {
-  if (i >= 0) {
-    if (j < i) {
-      if (sortArray[j] > sortArray[j + 1]) {
-        // swap numbers
-        let temp = sortArray[j];
-        sortArray[j] = sortArray[j + 1];
-        sortArray[j + 1] = temp;
-        return { i, j: j + 1 };
-      } else {
-        return { i, j: j + 1 };
-      }
-    } else {
-      return { i: i - 1, j: 0 };
-    }
-  }
-  sorting = false;
-  // noLoop();
-  return { i, j };
 }
 
 function mergeSort(low, high) {
@@ -89,61 +39,71 @@ function merge(low, middle, high) {
       sortArray[mergeCount] = copy[mid + 1];
       mid++;
     }
+    snapshot();
     mergeCount++;
   }
 
   if (low > middle) {
     while (mid < high) {
       sortArray[mergeCount] = copy[mid + 1];
+      snapshot();
       mid++;
       mergeCount++;
     }
   } else {
     while (low <= middle) {
       sortArray[mergeCount] = copy[low];
+      snapshot();
       low++;
       mergeCount++;
     }
   }
 }
 
-let count1, count2;
+function quickSort() {}
 
-function setup() {
-  console.log("SETUP");
-  createCanvas(1000, 500);
-  frameRate(200);
-  createRandomArray(500);
-  bubbleSort();
-  console.log(state);
-
-  // noLoop();
-
-  slider = createSlider(5, state.length - 1, 0, 1);
-  slider.position(5, 600);
-  slider.style("width", "1000px");
-  // slider.touchMoved(() => {
-  //   createRandomArray(slider.value());
-  //   draw();
-  // });
+let state = [];
+function snapshot() {
+  state.push(sortArray.map((value) => value));
 }
 
-let count = 0;
+let sortArray = [];
+function createRandomArray(length) {
+  sortArray = [];
+  for (let i = 0; i < length; i++) {
+    sortArray.push(Math.random());
+  }
+}
+
+// p5 setup function
+function setup() {
+  createCanvas(1000, 500);
+  frameRate(60);
+  createRandomArray(elementQtySlider.value);
+  noLoop();
+}
+
+// p5 draw function
 function draw() {
   console.log("DRAW");
-  background(210, 210, 210);
-  count = slider.value();
-  sortArray = state[count];
+
+  stepGraph();
   drawGraph();
-  // noLoop();
 }
 
+function stepGraph() {
+  if (state.length > 0 && slider.value < state.length) {
+    sortArray = state[slider.value];
+    slider.value++;
+  }
+}
+
+const BAR_SPACING = 1;
 function drawGraph() {
+  background(210, 210, 210);
   let barWidth =
     (width - (BAR_SPACING * sortArray.length + BAR_SPACING)) / sortArray.length;
-
   for (let i = 0; i < sortArray.length; i++) {
-    // stroke(30);
     noStroke();
     fill(51, 102, 255);
     rect(
@@ -154,3 +114,139 @@ function drawGraph() {
     );
   }
 }
+
+let selectedSort = "";
+function startAnimation() {
+  switch (selectedSort) {
+    case "bubble-sort":
+      bubbleSort();
+      sorting = true;
+      break;
+    case "merge-sort":
+      mergeSort(0, sortArray.length - 1);
+      sorting = true;
+      break;
+    default:
+      return;
+  }
+
+  slider.max = state.length - 1;
+  loop();
+}
+
+let slider = document.getElementById("progress-bar");
+slider.addEventListener("click", () => {});
+slider.oninput = function () {
+  noLoop();
+  sortArray = state[slider.value];
+  drawGraph();
+  if (state.length > 0) {
+    pauseButton.classList.add("selected");
+    startButton.classList.remove("selected");
+  }
+};
+
+let elementQty = document.getElementById("elements");
+let elementQtySlider = document.getElementById("qty-slider");
+elementQty.innerHTML = elementQtySlider.value;
+elementQtySlider.oninput = function () {
+  // pause p5 draw loop
+  noLoop();
+  // reset state array and slider
+  state = [];
+  slider.value = 0;
+  slider.max = 0;
+  sorting = false;
+  pauseButton.classList.remove("selected");
+  startButton.classList.remove("selected");
+  // create new random array and draw it on canvas
+  elementQty.innerHTML = this.value;
+  createRandomArray(this.value);
+  drawGraph();
+};
+
+// let frSlider = document.getElementById("frame-rate");
+// frSlider.oninput = function () {
+//   console.log("FR", this.value);
+//   frameRate(2);
+// };
+
+let sorting = false;
+let startButton = document.getElementById("start-button");
+startButton.addEventListener("click", () => {
+  if (sorting) {
+    loop();
+  } else {
+    startAnimation();
+  }
+  if (state.length > 0) {
+    startButton.classList.add("selected");
+    pauseButton.classList.remove("selected");
+  }
+});
+
+let pauseButton = document.getElementById("pause-button");
+pauseButton.addEventListener("click", () => {
+  noLoop();
+  if (state.length > 0) {
+    pauseButton.classList.add("selected");
+    startButton.classList.remove("selected");
+  }
+});
+
+let sortButtons = [];
+
+let bubbleSortButton = document.getElementById("bubble-sort");
+sortButtons.push(bubbleSortButton);
+bubbleSortButton.addEventListener("click", () => {
+  selectSortMethod("bubble-sort");
+});
+
+let mergeSortButton = document.getElementById("merge-sort");
+sortButtons.push(mergeSortButton);
+mergeSortButton.addEventListener("click", () => {
+  selectSortMethod("merge-sort");
+});
+
+function selectSortMethod(method) {
+  selectedSort = method;
+  sortButtons.forEach((button) => {
+    if (button.id === method) {
+      button.classList.add("selected");
+    } else {
+      button.classList.remove("selected");
+    }
+  });
+  if (sorting) {
+    state = [];
+    slider.value = 0;
+    slider.max = 0;
+    sorting = false;
+    createRandomArray(elementQtySlider.value);
+    drawGraph();
+    noLoop();
+    pauseButton.classList.remove("selected");
+    startButton.classList.remove("selected");
+  }
+}
+
+// function bubbleSortStep(i, j) {
+//   if (i >= 0) {
+//     if (j < i) {
+//       if (sortArray[j] > sortArray[j + 1]) {
+//         // swap numbers
+//         let temp = sortArray[j];
+//         sortArray[j] = sortArray[j + 1];
+//         sortArray[j + 1] = temp;
+//         return { i, j: j + 1 };
+//       } else {
+//         return { i, j: j + 1 };
+//       }
+//     } else {
+//       return { i: i - 1, j: 0 };
+//     }
+//   }
+//   sorting = false;
+//   // noLoop();
+//   return { i, j };
+// }
