dc9a6820c977b4940f98c7729b4a1e7ff1d18bf5
Jacob Wallin <jacobwallin@gmail.com>
2020-06-18 23:55:06 -0500

    updated insertion animation

--- src/algorithms/insertion.js
+++ src/algorithms/insertion.js
@@ -5,28 +5,58 @@ let animationState = [];
 export default function insertionSort(array) {
   animationState = [];
   snapshot(animationState, array);
-  for (let i = 1; i < array.length; i++) {
+  for (let i = 1; i < array.length - 1; i++) {
     let swapIndex = i;
-    array[i].status = "SELECTED";
-    snapshot(animationState, array);
     for (let j = i - 1; j >= 0; j--) {
+      array[swapIndex].status = "SELECTED";
       if (array[j].num > array[swapIndex].num) {
+        // take snapshot showing swap
+        array[j].status = "SWAPPED";
+        snapshot(animationState, array);
+        array[swapIndex].status = "UNSORTED";
+        array[j].status = "UNSORTED";
+        // swap elements
         const temp = array[swapIndex];
         array[swapIndex] = array[j];
         array[j] = temp;
-
-        array[swapIndex].status = "UNSORTED";
-        array[j].status = "SELECTED";
         swapIndex--;
-        snapshot(animationState, array);
       } else {
+        snapshot(animationState, array);
         array[swapIndex].status = "UNSORTED";
         break;
       }
     }
     array[0].status = "UNSORTED";
   }
-  array.forEach((element) => (element.status = "SORTED"));
+
+  // last pass through array is separated to mark elements as sorted one by one
+  let swapIndex = array.length - 1;
+  for (let j = swapIndex - 1; j > 0; j--) {
+    array[swapIndex].status = "SELECTED";
+    if (array[j].num > array[swapIndex].num) {
+      // take snapshot showing swap
+      array[j].status = "SWAPPED";
+      snapshot(animationState, array);
+      array[swapIndex].status = "UNSORTED";
+      array[j].status = "UNSORTED";
+      // swap elements
+      const temp = array[swapIndex];
+      array[swapIndex] = array[j];
+      array[j] = temp;
+      array[swapIndex].status = "SORTED";
+      swapIndex--;
+    } else {
+      snapshot(animationState, array);
+      array[swapIndex].status = "UNSORTED";
+      break;
+    }
+  }
+
+  for (let i = swapIndex; i >= 0; i--) {
+    array[i].status = "SORTED";
+  }
+
   snapshot(animationState, array);
+
   return animationState;
 }
